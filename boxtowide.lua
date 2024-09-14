09ff64f45d704ce5a961676eb723a20876f89004
Sam <17427046+Samillion@users.noreply.github.com>
2024-09-14 22:58:01 +0300

    Small improvements and cleanup

--- boxtowide.lua
+++ boxtowide.lua
@@ -1,23 +1,25 @@
 --[[
 
 This script changes the aspect ratio of all 4:3 videos to 16:9.
-
-It does so by first checking if the file is a video by doing a
-file extension check or if it's a video stream with ytdl, then
-checks the video parameter aspect ratio, if the aspect ratio is
-4:3 it changes it to 16:9 then ends the script.
-
 Big thanks to Argon-, I couldn't have done it without his help.
-Also thanks to autoload authors, specifically for these parts:
-function Set(t), EXTENSIONS, function get_extension(path)
+
+More info: https://github.com/Samillion/mpv-boxtowide
 
 --]]
 
 -- Set this to false to disable the script
 local ratioFix = true
 
--- Set this to false to disable resetting aspect ratio to original for images that could be in video playlists/folders.
+-- True: Do not alter image files aspect ratio.
 local imagesFix = true
+
+local extVideos = {
+	'mkv', 'avi', 'mp4', 'ogv', 'webm', 'rmvb', 'flv', 'wmv', 'mpeg', 'mpg', 'm4v', '3gp', 'ts'
+}
+
+local extImages = {
+    'jpg', 'jpeg', 'png', 'tif', 'tiff', 'gif', 'webp', 'svg', 'bmp'
+}
 
 local msg = require 'mp.msg'
 local utils = require 'mp.utils'
@@ -28,21 +30,10 @@ local function Set (t)
 	return set
 end
 
-local EXTENSIONS = Set {
-	'mkv', 'avi', 'mp4', 'ogv', 'webm', 'rmvb', 'flv', 'wmv', 'mpeg', 'mpg', 'm4v', '3gp', 'ts'
-}
+extVideos, extImages = Set(extVideos), Set(extImages)
 
-local EXTENSIONS_IMAGES = Set {
-    'jpg', 'jpeg', 'png', 'tif', 'tiff', 'gif', 'webp', 'svg', 'bmp'
-}
-
-local function getExtension(path)
-	match = string.match(path, "%.([^%.]+)$" )
-	if match == nil then
-		return ''
-	else
-		return match
-	end
+local function getExtension(value)
+    return value:match("%.([^%.]+)$") or ''
 end
 
 local function setBoxRatio(name, value)
@@ -55,7 +46,6 @@ local function setBoxRatio(name, value)
 		end
 		
 		mp.unobserve_property(setBoxRatio)
-		msg.info("Finished check, script no longer running.")
 	end
 end
 
@@ -63,18 +53,15 @@ local function prepareRatioCheck()
 	local path = mp.get_property("path", "")
 	local dir, filename = utils.split_path(path)
 
-	if EXTENSIONS[string.lower(getExtension(filename))] or string.match(string.lower(path), "^(https?://)") then
+	if extVideos[string.lower(getExtension(filename))] or string.match(string.lower(path), "^%a+://") then
 		mp.set_property("video-aspect-override", "-1")
 		msg.info("Aspect-ratio has been reset to default to initialize ratio check.")
 		
 		mp.observe_property("video-params/aspect", "number", setBoxRatio)
 		msg.info("Observing aspect-ratio value...")
-		
-	elseif EXTENSIONS_IMAGES[string.lower(getExtension(filename))] and imagesFix then
+	elseif extImages[string.lower(getExtension(filename))] and imagesFix then
 		mp.set_property("video-aspect-override", "-1")
 		msg.info("Image file detected. Aspect ratio has been reset to original.")
-	else
-		msg.info("Not video file, script didn't run.")
 	end
 end
 

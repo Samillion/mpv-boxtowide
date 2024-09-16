8ccc790e60def0f23043995488fbd2444440dd54
Sam <17427046+Samillion@users.noreply.github.com>
2024-09-16 10:26:57 +0300

    Add extensions, reduce msg.info noise and small changes.
    - Add more video file extensions.
    - Add more image file extensions to more accurately ignore them.
    - Reduce msg.info, only output when script actually does something.
    - Rename imagesFix to ignoreImages to be self explanatory 
    - Other small changes

--- boxtowide.lua
+++ boxtowide.lua
@@ -7,19 +7,18 @@ More info: https://github.com/Samillion/mpv-boxtowide
 
 --]]
 
--- Set this to false to disable the script
-local ratioFix = true
-
--- True: Do not alter image files aspect ratio.
-local imagesFix = true
-
 local extVideos = {
-	'mkv', 'avi', 'mp4', 'ogv', 'webm', 'rmvb', 'flv', 'wmv', 'mpeg', 'mpg', 'm4v', '3gp', 'ts'
+	'3g2', '3gp', 'avi', 'flv', 'm2ts', 'm4v', 'mj2', 'mkv', 'mov',
+	'mp4', 'mpeg', 'mpg', 'ogv', 'rmvb', 'ts', 'webm', 'wmv', 'y4m'
 }
 
-local extImages = {
-    'jpg', 'jpeg', 'png', 'tif', 'tiff', 'gif', 'webp', 'svg', 'bmp'
+local extImages = {	
+	'avif', 'bmp', 'gif', 'j2k', 'jp2', 'jpeg', 'jpg', 'jxl', 'png',
+	'svg', 'tga', 'tif', 'tiff', 'webp'
 }
+
+-- true to ignore changing aspect ratio for images
+local ignoreImages = true
 
 local msg = require 'mp.msg'
 local utils = require 'mp.utils'
@@ -30,8 +29,6 @@ local function Set (t)
 	return set
 end
 
-extVideos, extImages = Set(extVideos), Set(extImages)
-
 local function getExtension(value)
     return value:match("%.([^%.]+)$") or ''
 end
@@ -41,30 +38,24 @@ local function setBoxRatio(name, value)
 		if (value >= 1.28) and (value <= 1.39) then
 			mp.set_property("video-aspect-override", "16:9")
 			msg.info("Aspect-ratio changed from 4:3 to 16:9")
-		else
-			msg.info("Aspect-ratio unchanged, video is not 4:3.")
 		end
 		
 		mp.unobserve_property(setBoxRatio)
 	end
 end
 
+extVideos, extImages = Set(extVideos), Set(extImages)
+
 local function prepareRatioCheck()
 	local path = mp.get_property("path", "")
 	local dir, filename = utils.split_path(path)
 
-	if extVideos[string.lower(getExtension(filename))] or string.match(string.lower(path), "^%a+://") then
+	if extVideos[string.lower(getExtension(filename))] or string.match(path, "^%a+://") then
 		mp.set_property("video-aspect-override", "-1")
-		msg.info("Aspect-ratio has been reset to default to initialize ratio check.")
-		
 		mp.observe_property("video-params/aspect", "number", setBoxRatio)
-		msg.info("Observing aspect-ratio value...")
-	elseif extImages[string.lower(getExtension(filename))] and imagesFix then
+	elseif extImages[string.lower(getExtension(filename))] and ignoreImages then
 		mp.set_property("video-aspect-override", "-1")
-		msg.info("Image file detected. Aspect ratio has been reset to original.")
 	end
 end
 
-if ratioFix then
-	mp.register_event("start-file", prepareRatioCheck)
-end
+mp.register_event("start-file", prepareRatioCheck)

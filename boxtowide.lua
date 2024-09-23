0b926893af0e68842b8615a0aef24f824786f584
Sam <17427046+Samillion@users.noreply.github.com>
2024-09-23 06:41:38 +0300

    Ignore animated image files (apng, avif, gif, webp)
    This is what happens when I try to simplify too much xD . Though this is definitely simpler than what it was before.
    
    Also, don't use video-reconfig, as it runs the script more than once, needlessly. Use start-file instead, and observe the aspect ratio property.

--- boxtowide.lua
+++ boxtowide.lua
@@ -7,26 +7,38 @@ More info: https://github.com/Samillion/mpv-boxtowide
 
 --]]
 local msg = require 'mp.msg'
+local utils = require 'mp.utils'
 
 local function setBoxRatio(name, value)
 	if value ~= nil then
-		if (value >= 1.28) and (value <= 1.39) then
-			mp.set_property("file-local-options/video-aspect-override", "16:9")
-			msg.info("Aspect-ratio changed from 4:3 to 16:9")
+		if not mp.get_property_native('current-tracks/video/image') and not mp.get_property_native('current-tracks/video/albumart') then
+			if mp.get_property_native('vid') then
+				if (value >= 1.28) and (value <= 1.39) then
+					mp.set_property("file-local-options/video-aspect-override", "16:9")
+					msg.info("Aspect-ratio changed from 4:3 to 16:9")
+				end
+			end
 		end
 		
 		mp.unobserve_property(setBoxRatio)
 	end
 end
 
+local function Set(t)
+    local set = {}
+    for _, v in pairs(t) do set[v] = true end
+    return set
+end
+
 local function prepareCheck()
 	local path = mp.get_property("path", "")
-
-	if not mp.get_property_native('current-tracks/video/image') and not mp.get_property_native('current-tracks/video/albumart') then
-		if mp.get_property_native('vid') or string.match(path, "^%a+://") then
-			mp.observe_property("video-params/aspect", "number", setBoxRatio)
-		end
+	local dir, filename = utils.split_path(path)
+	local ext = filename:lower():match("%.([^%.]+)$") or ''
+	local blacklist = { 'apng', 'avif', 'gif', 'webp' }
+	
+	if not Set(blacklist)[ext] then
+		mp.observe_property("video-params/aspect", "number", setBoxRatio)
 	end
 end
 
-mp.register_event("video-reconfig", prepareCheck)
+mp.register_event("start-file", prepareCheck)

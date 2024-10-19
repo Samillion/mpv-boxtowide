6b4926a3dea6617cebbe97cce4ef464e4dd971bd
Sam <17427046+Samillion@users.noreply.github.com>
2024-10-19 20:13:27 +0300

    Spacing

--- boxtowide.lua
+++ boxtowide.lua
@@ -1,25 +1,25 @@
 --[[
 
-This script changes the aspect ratio of all 4:3 videos to 16:9.
-Big thanks to Argon-, I couldn't have done it without his help.
+    This script changes the aspect ratio of all 4:3 videos to 16:9.
+    Big thanks to Argon-, I couldn't have done it without his help.
 
-More info: https://github.com/Samillion/mpv-boxtowide
+    More info: https://github.com/Samillion/mpv-boxtowide
 
 --]]
 local msg = require 'mp.msg'
 
 local function setBoxRatio(name, value)
-	if value ~= nil then
-		local videoTrack = mp.get_property_native('current-tracks/video')
-		
-		if videoTrack and not videoTrack.image and not videoTrack.albumart then
-			if value >= 1.28 and value <= 1.39 then
-				mp.set_property("file-local-options/video-aspect-override", "16:9")
-				msg.info("Aspect-ratio changed from 4:3 to 16:9")
-				mp.unobserve_property(setBoxRatio)
-			end
-		end
-	end
+    if value ~= nil then
+        local videoTrack = mp.get_property_native('current-tracks/video')
+
+        if videoTrack and not videoTrack.image and not videoTrack.albumart then
+            if value >= 1.28 and value <= 1.39 then
+                mp.set_property("file-local-options/video-aspect-override", "16:9")
+                msg.info("Aspect-ratio changed from 4:3 to 16:9")
+                mp.unobserve_property(setBoxRatio)
+            end
+        end
+    end
 end
 
 local function Set(t)
@@ -29,16 +29,16 @@ local function Set(t)
 end
 
 local function prepareCheck()
-	local path = mp.get_property("path", "")
-	local ext = path:match("%.([^%.]+)$") or 'nomatch'
-	local whitelist = Set {
-		'3g2', '3gp', 'avi', 'flv', 'm2ts', 'm4v', 'mj2', 'mkv', 'mov',
-		'mp4', 'mpeg', 'mpg', 'ogv', 'rmvb', 'ts', 'webm', 'wmv', 'y4m'
-	}
-	
-	if whitelist[ext:lower()] or path:match('^%a+://') then
-		mp.observe_property("video-params/aspect", "number", setBoxRatio)
-	end
+    local path = mp.get_property("path", "")
+    local ext = path:match("%.([^%.]+)$") or 'nomatch'
+    local whitelist = Set {
+        '3g2', '3gp', 'avi', 'flv', 'm2ts', 'm4v', 'mj2', 'mkv', 'mov',
+        'mp4', 'mpeg', 'mpg', 'ogv', 'rmvb', 'ts', 'webm', 'wmv', 'y4m'
+    }
+
+    if whitelist[ext:lower()] or path:match('^%a+://') then
+        mp.observe_property("video-params/aspect", "number", setBoxRatio)
+    end
 end
 
 mp.register_event("start-file", prepareCheck)
